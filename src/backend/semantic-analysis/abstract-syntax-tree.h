6250edb2f75dbb69696590429e7b1c6aa2f9076d
Tomas Camilo Gay Bare <tomigayba02yt@gmail.com>
2023-05-31 18:52:58 -0300

    fix punctutation

--- src/backend/semantic-analysis/abstract-syntax-tree.h
+++ src/backend/semantic-analysis/abstract-syntax-tree.h
@@ -10,29 +10,29 @@ typedef struct SubstitutesNoNumNode SubstitutesNoNumNode;
 enum NumerationType{
 	NUM,
 	NONUM
-}
+};
 
 enum TeamType{
 	TEAMNAME,
 	NOTEAMNAME
-}
+};
 
 enum FormationNumberType{
 	FINAL,
 	NOFINAL
-}
+};
 
 enum MetadataType{
 	DATE,
 	RESULT,
 	COMPLETE,
 	EMPTY
-}
+};
 
 
 struct InitialNode{
 	InfoNode * info;
-}
+};
 
 struct InfoNode{
 	NumerationType numeration;
@@ -40,75 +40,75 @@ struct InfoNode{
 	FormationNode * formation;
 	LineupNode * lineup;
 	MetadataNode * metadata;
-}
+};
 
 struct TeamNode{
 	TeamType teamType;
 	char * teamName;
 	int teamNumber;
-}
+};
 
 struct FormationNode{
 	FormationNumberNode * formationNumber;
-}
+};
 
 struct FormationNumberNode{
 	FormationNumberType formationNumberType;
 	char * formationNumber;
 	FormationNumberNode * nextFormationNumber;
-}
+};
 
 struct LineupNode{
 	PlayerInfoNode * playerInfo;
 	SubstitutesNode * substitutes;
-}
+};
 
 struct PlayerInfoNode{
 	char * playerName;
 	int playerNumber;
 	PlayerInfoNode * nextPlayerInfo;
-}
+};
 
 struct SubstitutesNode{
 	char * substituteName;
 	int substituteNumber;
 	SubstitutesNode * nextSubstitute;
-}
+};
 
 struct LineupNoNumNode{
 	PlayerInfoNoNumNode * playerInfoNoNum;
 	SubstitutesNoNumNode * substitutesNoNum;
-}
+};
 
 struct PlayerInfoNoNumNode{
 	char * playerName;
 	PlayerInfoNoNumNode * nextPlayerInfoNoNum;
-}
+};
 
 struct SubstitutesNoNumNode{
 	char * substituteName;
 	SubstitutesNoNumNode * nextSubstituteNoNum;
-}
+};
 
 struct MetadataNode{
 	MetadataType metadataType;
 	MatchDateNode * matchDate;
 	MatchResultNode * matchResult;
-}
+};
 
 enum MetadataType{
 	DATE,
 	RESULT,
 	COMPLETE,
 	EMPTY
-}
+};
 
 struct MatchDateNode{
 	char * date;
-}
+};
 
 struct MatchResultNode{
 	char * result;
-}
+};
 
 #endif
